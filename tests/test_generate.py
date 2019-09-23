61d105fbe9a9e63136be3c2abcf71928b9bfd47e
Merwane Draï <merwanedr@gmail.com>
2019-09-23 14:36:07 +0100

    bch network test

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -2,6 +2,7 @@ import pytest
 import re
 from config import CRYPTO_NETWORK
 from resources.btc.generate import GenerateBtcKey
+from resources.bch.generate import GenerateBchKey
 
 def test_btc_key():
     btc_key = GenerateBtcKey()
@@ -10,3 +11,8 @@ def test_btc_key():
         assert re.match('^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$', addr)
     elif CRYPTO_NETWORK == 'testnet':
         assert re.match('^[2mn][1-9A-HJ-NP-Za-km-z]{26,35}', addr)
+
+def test_bch_key():
+    bch_key = GenerateBchKey()
+    addr = bch_key.generate_std_addr()
+    assert re.match('^((bitcoincash|bchreg|bchtest):)?(q|p)[a-z0-9]{41}$', addr)
--- tests/test_network.py
+++ tests/test_network.py
@@ -1,11 +1,20 @@
 import pytest
 import bit
+import bitcash
 from resources.btc.network import btc_network
+from resources.bch.network import bch_network
 from config import CRYPTO_NETWORK
 
-def test_network():
+def test_btc_network():
     key = btc_network()
     if CRYPTO_NETWORK == 'testnet':
         assert type(key) == bit.wallet.PrivateKeyTestnet
     elif CRYPTO_NETWORK == 'mainnet':
         assert type(key) == bit.wallet.PrivateKey
+
+def test_bch_network():
+    key = bch_network()
+    if CRYPTO_NETWORK == 'testnet':
+        assert type(key) == bitcash.wallet.PrivateKeyTestnet
+    elif CRYPTO_NETWORK == 'mainnet':
+        assert type(key) == bitcash.wallet.PrivateKey
